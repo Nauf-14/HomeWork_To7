5f34b7b1329b1e364be011bbed00bb9c9428f68a
Naufal <nauf1207@mail.ru>
2023-03-29 23:18:16 +0600

    двумерный массив размером m×n,заполненный случайными вещественными числами.

---
+++ Task_47/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+void InputMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Math.Round(new Random().NextDouble()* (-10) + 10, 1);
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+double[,] array = new double[size[0], size[1]];
+InputMatrix(array);
+PrintMatrix(array);
+
